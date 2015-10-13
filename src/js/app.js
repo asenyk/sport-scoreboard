8f063293d0bb8e03c04c4de123c09329652cb7a8
asenyk <bloody.junior@gmail.com>
2015-10-13 19:46:52 +0300

    api call

--- src/js/app.js
+++ src/js/app.js
@@ -3,43 +3,50 @@
 
     var app = angular.module('scoreboard', []);
 
-    app.controller('TeamsController', function ($http, $sce) {
+    app.controller('Football', function ($http, $sce) {
+
         var vm = this;
+
         $http.get('http://footballbet.com.ua/api/teams/').
-        success(function(data, status, headers, config) {
+        success(function (data, status, headers, config) {
             vm.teamList = data.result;
         }).
-        error(function(data, status, headers, config) {
+        error(function (data, status, headers, config) {
             alert('Error !');
         });
+
+        $http.get('http://footballbet.com.ua/api/championships/').
+        success(function (data, status, headers, config) {
+            vm.champList = data.result;
+        }).
+        error(function (data, status, headers, config) {
+            alert('Error !');
+        });
+
         vm.trustAsHtml = function (text) {
             return $sce.trustAsHtml(text);
         };
-        //
-        //vm.loadTeam = function () {
-        //    console.log(vm.team);
-        //}
+
+        vm.loadTeams = function () {
+            vm.champTeamTable = _.filter(vm.teamList, {'id_championship': vm.championship.id_championship});
+            console.log(vm.championship.id_championship);
+            console.log(vm.champTeamTable);
+        };
 
     });
 
-    app.controller('ChampionshipsController', function ($http) {
-        var vm = this;
-        $http.get('http://footballbet.com.ua/api/championships/').
-        success(function(data, status, headers, config) {
-            vm.champList= data.result;
-        }).
-        error(function(data, status, headers, config) {
-            alert('Error !');
-        });
-    });
+    //
+    //vm.loadTeam = function () {
+    //    console.log(vm.team);
+    //}
 
     app.controller('MatchesController', function ($http) {
         var vm = this;
         $http.get('http://footballbet.com.ua/api/matches/').
-        success(function(data, status, headers, config) {
-            vm.matches= data.result;
+        success(function (data, status, headers, config) {
+            vm.matches = data.result;
         }).
-        error(function(data, status, headers, config) {
+        error(function (data, status, headers, config) {
             alert('Error !');
         });
     });
